15dca01c5f68bedf3390fb25048e62e2fa48edfa
umang <iumangsharma001@gmail.com>
2020-05-13 21:50:30 +0530

    Added Location Access.

--- app/src/main/java/com/example/weather/WeatherController.java
+++ app/src/main/java/com/example/weather/WeatherController.java
@@ -1,11 +1,20 @@
 package com.example.weather;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.weather.R;
 
@@ -13,6 +22,7 @@ import com.example.weather.R;
 public class WeatherController extends AppCompatActivity {
 
 
+     final int REQUEST_CODE = 123;
     final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
 
     final String APP_ID = "e72____PLEASE_REPLACE_ME_____13";
@@ -21,13 +31,14 @@ public class WeatherController extends AppCompatActivity {
 
     final float MIN_DISTANCE = 1000;
 
+    String location_provider = LocationManager.NETWORK_PROVIDER;
 
-    TextView tempView , locationView;
+
+    TextView tempView, locationView;
     Button changeCityButton;
     ImageView weatherView;
-
-
-
+    LocationManager locationManager;
+    LocationListener locationListener;
 
 
     @Override
@@ -36,15 +47,74 @@ public class WeatherController extends AppCompatActivity {
         setContentView(R.layout.weather_controller_layout);
 
         locationView = findViewById(R.id.locationTV);
-        tempView =findViewById(R.id.tempTV);
-        changeCityButton =findViewById(R.id.changeCityButton);
+        tempView = findViewById(R.id.tempTV);
+        changeCityButton = findViewById(R.id.changeCityButton);
         weatherView = findViewById(R.id.weather_image);
 
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("weather", "onResume() called");
+        getWeatherForCurrentLocation();
 
+    }
 
+    private void getWeatherForCurrentLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Weather", "onLocationChanged() callback received");
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("weather", "onProviderDisables() callback received ");
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE);
+            return;
+        }
+        locationManager.requestLocationUpdates(location_provider, MIN_TIME, MIN_DISTANCE, locationListener);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == REQUEST_CODE){
+
+            if (grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
+                Log.d("weather", "onRequestPermissionResult() :permission Granted");
+            }else
+                Log.d("weather","onRequestPermissionResult() :permission Denied");
+        }
+    }
 
 
 
